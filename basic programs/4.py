f64afb7feb4542a9fb05cb56eb3247a1421397ab
Akhil Gokuldas <47827849+AkhilDasPanicker@users.noreply.github.com>
2019-03-20 14:02:10 +0530

    python | basics | akhil gokuldas

--- basic programs/4.py
+++ basic programs/4.py
@@ -1,3 +1,5 @@
+#akhil gokuldas
+
 print 22/7.0
 print 22/7
 print 33.8/3
@@ -8,7 +10,6 @@ print  1*(2+5)-6/2+7+9-7-2+(9*2)
 print (1*2)+5-(6/2)+7+9-7-2+(9*2)
 print 7.0/3
 print 2!=5
-# print 1+2
 print 1+2
 print 1/2/3/5*69*7+1
 
